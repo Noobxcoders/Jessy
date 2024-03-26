42f397692164cc2a04496ff793e44ad49a7ceede
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2024-03-26 14:35:46 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from HarleyMusic import app
+from AnonXMusic import app
 from config import YOUTUBE_IMG_URL
 
 
